695e3c7f9fb01885a75bd20023e90856f3b2a5e6
Charles <charles.puelz@gmail.com>
2021-02-16 15:01:00 -0600

    some small changes

--- vary_Rp/optimal_vary_Rp.m
+++ vary_Rp/optimal_vary_Rp.m
@@ -3,11 +3,11 @@ clear all; close all;  clc;
 % pulsatile code below
 num_compliance_chambers = 5;
 num_resistors = 6;
-A0_vec = linspace(  0.00,     0.006, 50); %cross sectional area array in dm^2
+A0_vec = linspace(0.00,0.006, 5); %cross sectional area array in dm^2
 mean_pressure = zeros(num_compliance_chambers, length(A0_vec));
 mean_O2_concentration = zeros(num_compliance_chambers, length(A0_vec));
 mean_flow = zeros(num_resistors, length(A0_vec));
-Rp_vec = linspace(0.01, 4, 50);
+Rp_vec = linspace(0.01, 4, 5);
 for jj=1:length(Rp_vec) 
     Rp = Rp_vec(jj);
 for ii = 1:length(A0_vec)
@@ -16,11 +16,11 @@ for ii = 1:length(A0_vec)
     mean_pressure(:,ii) = compute_mean(T, dt, t_plot, P_plot);
     mean_O2_concentration(:,ii) = compute_mean(T, dt, t_plot, O2_plot);
     mean_flow(:,ii) = compute_mean(T, dt, t_plot, Q_plot);
-    clearvars -except RFe A0_vec A0 mean_pressure mean_O2_concentration mean_flow Rp_vec Rp optimal_size_plot jj ;    
+    clearvars -except RFe A0_vec A0 mean_pressure mean_O2_concentration mean_flow Rp_vec Rp optimal_size_plot jj;    
 end
-oxygen_delivery_vec=mean_O2_concentration(1,:) .* mean_flow(1,:);
-[max_ox_deliv,optimal_index]=max(mean_O2_concentration(1,:) .* mean_flow(1,:));
-optimal_size=A0_vec(optimal_index);
+oxygen_delivery_vec = mean_O2_concentration(1,:) .* mean_flow(1,:);
+[max_ox_deliv,optimal_index] = max(mean_O2_concentration(1,:) .* mean_flow(1,:));
+optimal_size = A0_vec(optimal_index);
 optimal_size_plot(jj) = optimal_size;
 end
 
