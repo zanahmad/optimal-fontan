576f6fc613510597d1238c07dbaf45c2f4863008
zanahmad <60706569+zanahmad@users.noreply.github.com>
2021-07-17 12:45:05 -0400

    Update plotting figures for prefenestration results

--- closed_fenestration/circ_out.m
+++ closed_fenestration/circ_out.m
@@ -1,27 +1,45 @@
 %filename: circ_out.m
 %script to plot results of computer simulation 
-%of the entire circulation.
+%of the Fontan circulation.
 
 %right ventricular compliance, pressures, and flows:
 figure(1)
-subplot(3,1,1),plot(t_plot,C_plot(iRV,:),'linewidth',3)
+plot(t_plot,C_plot(iRV,:).*1000,'linewidth',3)
 title('Compliance v. Time of RV')
 xlabel('t (min)')
-ylabel('C (L/mmHg)')
+ylabel('C (mL/mmHg)')
 xlim([klokmax*dt-5*T klokmax*dt])
-subplot(3,1,2),plot(t_plot,P_plot([iRV,isa,ipa],:),'linewidth',3)
-title('Pressure v Time (RV,sa,pa)')
+
+figure(100) 
+plot(t_plot, (1./C_plot(iRV,:))./1000, 'linewidth', 3)
+title('Elastance v. Time or RV')
+xlabel('t (min)')
+ylabel('E (mmHg/mL)')
+xlim([klokmax*dt-5*T klokmax*dt])
+
+
+figure(2)
+plot(t_plot,P_plot([iRV,isa,ipa],:),'linewidth',3)
+title('Pressure v. Time (RV,sa,pa)')
 xlabel('t (min)')
 ylabel('P (mmHg)')
 xlim([klokmax*dt-5*T klokmax*dt])
-subplot(3,1,3),plot(t_plot,Q_plot([jTr,jAo],:),'linewidth',3)
-title('Flow v Time (Tr and Ao)')
+id = legend("RV", "sa", "pa");
+legend boxoff
+set(gca,'fontsize',18)
+
+figure(3)
+plot(t_plot,Q_plot([jTr,jAo],:),'linewidth',3)
+title('Flow v. Time (Tr and Ao)')
 xlabel('t (min)')
 ylabel('Q (L/min)')
 xlim([klokmax*dt-5*T klokmax*dt])
+id = legend("Tr", "Ao");
+legend boxoff
+set(gca,'fontsize',18)
 
 %systemic and pulmonary flows:
-figure(2)
+figure(4)
 plot(t_plot,Q_plot([js,jp],:),'linewidth',3)
 title('Systemic and Pulmonary Flows')
 xlabel('t (min)')
@@ -29,10 +47,10 @@ ylabel('Q (L/min)')
 id = legend("Systemic", "Pulmonary");
 legend boxoff
 set(gca,'fontsize',18)
-xlim([klokmax*dt-1000*dt klokmax*dt])
+xlim([klokmax*dt - 5*T klokmax*dt])
 
-figure(3)%pressure-volume loops for right ventricle
-plot(V_plot(iRV,:),P_plot(iRV,:),'linewidth',3)
+figure(5)%pressure-volume loops for right ventricle
+plot(V_plot(iRV,klokmax-T/dt:klokmax),P_plot(iRV,klokmax-T/dt:klokmax),'linewidth',3)
 hold on
 plot([VRVd,.125],[0,EminRV*(.125-VRVd)],'linewidth', 3)
 hold on
@@ -40,7 +58,7 @@ plot([VRVd,.075],[0,EmaxRV*(.075-VRVd)],'linewidth', 3)
 hold on
 plot(.111,6.6,'r*','linewidth',3)
 plot(.0517,124,'b*','linewidth',3)
-title('Pressure Volume Loop RV')
+title('Right Ventricular Pressure-Volume Loop')
 xlabel('V (L)')
 ylabel('P (mmHg)')
 
